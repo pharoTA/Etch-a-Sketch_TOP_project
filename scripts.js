219c2d1bf837ce1a0c9ab354add64e5625046627
Pierre Haro <pierre.haroland@gmail.com>
2024-08-27 15:42:25 +0200

    Add hovering effect on square divs

--- scripts.js
+++ scripts.js
@@ -1,20 +1,24 @@
+const CONTAINER_HEIGHT = 40; // em
+
 const create_div = (gridSize) => {
     const squareDivContainer = document.querySelector("#grid_container");
-    const squareHeight = String(90 / gridSize);
+    const squareHeight = String(CONTAINER_HEIGHT / gridSize);
     for (i = 0; i < gridSize; i++) {
         const squareDivRowContainer = document.createElement("div");
         squareDivRowContainer.style.display = "flex";
         squareDivRowContainer.style.justifyContent = "stretch"
         squareDivRowContainer.style.width = "100%";
-        squareDivRowContainer.style.backgroundColor = "blue";
         squareDivContainer.appendChild(squareDivRowContainer);
         for (j = 0; j < gridSize; j++) {
             const squareDiv = document.createElement("div");
             squareDiv.setAttribute("class", "square-div");
             squareDiv.style.backgroundColor = "red";
-            squareDiv.style.minHeight = `${squareHeight}vh`;
-            squareDiv.style.minWidth = `${squareHeight}vw`;
+            squareDiv.style.minHeight = `${squareHeight}em`;
+            squareDiv.style.minWidth = `${squareHeight}em`;
             squareDiv.style.borderRadius = "10px";
+            squareDiv.addEventListener("mouseover", () => {
+                squareDiv.style.backgroundColor = "black";
+            });
             squareDivRowContainer.appendChild(squareDiv);
         }
     }
